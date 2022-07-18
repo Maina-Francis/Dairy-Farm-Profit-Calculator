dbf0eee18ace176b75b2b34489eb2628c8dc8876
Seb <sebastianfrancix@gmail.com>
2022-07-18 09:09:58 +0300

    targets html ids

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -10,3 +10,18 @@ function produceReport() {
 
   let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 
+  document.getElementById("outputData").innerHTML += "<Br>";
+
+  document.getElementById("outputData").innerHTML +=
+    "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
+  document.getElementById("outputData").innerHTML +=
+    "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
+  document.getElementById("outputData").innerHTML +=
+    "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
+  document.getElementById("outputData").innerHTML +=
+    "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
+
+  document.getElementById("outputData").innerHTML +=
+    "<p>Your total production per day is " + sumData + " litres per day</p>";
+
+  
